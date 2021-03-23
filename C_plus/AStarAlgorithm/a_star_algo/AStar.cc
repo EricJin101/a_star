4e6d84417fa5fc8b24c6c3adf8ac758dff3483e7
EricJin101 <foreveryouth93@163.com>
2021-03-23 16:44:17 +0800

    update: chile node check

--- C_plus/AStarAlgorithm/a_star_algo/AStar.cc
+++ C_plus/AStarAlgorithm/a_star_algo/AStar.cc
@@ -35,12 +35,78 @@ namespace aStarAlgorithm {
 
     }
 
+    int current_idx(int x, int y)
+    {
+        return x + mapInfo.boundary.first * y;
+    }
+
+    double manhattan_dis(int x1, int y1, int x2, int y2)
+    {
+        return abs(x1 - x2) + abs(y1 - y2);
+    }
+
+    bool check_in_close_list()
+    {
+
+    }
+
+    bool check_child_valid(path::PathList pathList)
+    {
+        if ((pathList.current.x > 0) && (pathList.current.y > 0 ) && globalMap[current_idx(pathList.current.x, pathList.current.y)].type != 'O')
+        {
+            if (globalMap[current_idx(pathList.current.x, pathList.current.y)].type != 'O')
+            {
+                findPath = true;
+            }
+            return true;
+        }
+        return false;
+    }
+
+    void add_child_to_openlist()
+    {
+        for(int i = 0; i>childList.size(); ++i)
+        {
+            // valid check
+            if (!check_child_valid(childList[i]))
+            {
+                continue;
+            }
+
+        }
+    }
+
+
+    void eight_direction(int x, int y)
+    {// 0 - 7, 8个方向。东、东北、北、西北、西、西南、南、东南
+        for (int i{0}; i < 8; ++i)
+        {
+            path::PathList point{};
+            cout << "sin: " << round(sin(i * 45* M_PI / 180)) << endl;
+            double delta_x = round(cos(i * 45* M_PI / 180));
+            double delta_y = round(sin(i * 45* M_PI / 180));
+            point.current.x = x + delta_x > 0 ? x + delta_x : -1;
+            point.current.y = y + delta_y > 0 ? y + delta_y : -1;
+            point.cost_g = globalMap[current_idx(x, y)].cost_g + 1.414;
+            point.cost_h = manhattan_dis(point.current.x, point.current.y, mapInfo.map_end.first, mapInfo.map_end.second);
+            point.cost_f = point.cost_g + point.cost_h;
+            childList.push_back(point);
+        }
+    }
+
+
     void core_aStar(int x, int y)
     {
+        // x y 即将处理的点，默认为起点
         cout << "findPath" << findPath << endl;
         data_prepare();
         initialize_root_node();
         cout << "done " << endl;
+        while (!findPath)
+        {
+            double g = globalMap[mapInfo.map_start.first + mapInfo.map_start.second * 5].cost_g;
+            eight_direction(x, y);
+        }
 
     }
 }
--- C_plus/AStarAlgorithm/a_star_algo/AStar.h
+++ C_plus/AStarAlgorithm/a_star_algo/AStar.h
@@ -4,14 +4,17 @@
 #ifndef ASTAR_ASTAR_H
 #define ASTAR_ASTAR_H
 #include "../utils/path.h"
+#include <cmath>
 
 namespace eric {
 namespace aStarAlgorithm{
         typedef vector<path::PathList> open_list;
         typedef vector<path::PathList> close_list;
+        typedef vector<path::PathList> child_list;
         bool findPath;
         path::global_map globalMap;
         path::map_info mapInfo;
+        child_list childList;
         static void Init();
 }
 }
